f7e3449168cb232a0c5435c081f0172539835d46
vinball23 <63390247+vinball23@users.noreply.github.com>
2021-12-02 18:10:10 +0700

    Tugas Laravel Bagian 5

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+    use Sluggable;
 
-    // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
 
@@ -43,4 +43,20 @@ class Post extends Model
     public function author() {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
+
+
